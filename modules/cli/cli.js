b111985c6da9d23ee7221b2a3bfaf169bab681e0
lithium-lx <lx.staticservices@gmail.com>
2021-08-04 23:13:02 +0200

    moved cli to a module

---
+++ modules/cli/cli.js
@@ -0,0 +1,40 @@
+const { client } = require('../../main');
+const rl = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: `${client.config['botName']}> `
+})
+
+module.exports.onLoad = async function () {
+    //CLI
+    try {
+        client.on('cliReady', () => {
+            rl.prompt();
+        });
+    } catch (err) {
+        console.error(err);
+    }
+    rl.on('line', (line) => {
+        client.tline = line.trim();
+        const args = line.trim().split(" ");
+        const cmd = args.shift();
+        switch (cmd) {
+            case 'emit':
+                //This can emit events and passes cl args to a client attribute.
+                //This can be used to exploit event calls as command calls.
+                const eevent = args.shift();
+                client.args = args;
+                client.emit(eevent);
+                console.log(`Emitted ${eevent}`);
+                break;
+            default:
+                console.log(`'${cmd}' isn't a valid command`);
+                break;
+        }
+        rl.prompt();
+    }).on('close', () => {
+        client.destroy();
+        console.log('\nClient destroyed, exiting process. Bye Bye!');
+        process.exit(0);
+    });
+};
